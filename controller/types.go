46132058c245ccf178bcc3eb8d97bc71119744e2
Smile <bagdanmyha@mail.ru>
2025-11-16 19:20:35 +0300

    Main types of controller

--- controller/types.go
+++ controller/types.go
@@ -1,1 +1,37 @@
 package controller
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type CommandType string
+
+type CommandSource string
+
+type ModuleConstructor func(config map[string]any) (any, string, string, error) // Instance Name Version Error
+
+type CommandResult struct {
+	Command   Command
+	Result    any
+	Error     error
+	Timestamp time.Time
+}
+
+type Command struct {
+	ID       uuid.UUID
+	Type     CommandType
+	Source   CommandSource
+	Sender   string
+	Executor string // moduleName.method
+	Data     map[string]any
+	Chanback chan CommandResult
+}
+
+type Module struct {
+	Name     string
+	Instance any
+	Version  string
+	Config   map[string]any
+}
